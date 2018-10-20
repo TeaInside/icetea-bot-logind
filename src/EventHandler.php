8ffce404a9e01a222389ae16e0ee128f2ef729d8
Ammar Faizi <ammarfaizi2@gmail.com>
2018-10-20 21:14:17 +0700

    Initial commit

---
+++ src/EventHandler.php
@@ -0,0 +1,47 @@
+<?php
+
+class EventHandler extends \danog\MadelineProto\EventHandler
+{
+    public function __construct($MadelineProto)
+    {
+        parent::__construct($MadelineProto);
+    }
+    public function onAny($update)
+    {
+        \danog\MadelineProto\Logger::log("Received an update of type ".$update['_']);
+    }
+    public function onLoop()
+    {
+        \danog\MadelineProto\Logger::log("Working...");
+    }
+    public function onUpdateNewChannelMessage($update)
+    {
+        $this->onUpdateNewMessage($update);
+    }
+    public function onUpdateNewMessage($update)
+    {
+        if (isset($update['message']['out']) && $update['message']['out']) {
+            return;
+        }
+        $res = json_encode($update, JSON_PRETTY_PRINT);
+        if ($res == '') {
+            $res = var_export($update, true);
+        }
+
+        try {
+            $this->messages->sendMessage(['peer' => $update, 'message' => $res, 'reply_to_msg_id' => $update['message']['id'], 'entities' => [['_' => 'messageEntityPre', 'offset' => 0, 'length' => strlen($res), 'language' => 'json']]]);
+        } catch (\danog\MadelineProto\RPCErrorException $e) {
+            $this->messages->sendMessage(['peer' => '@danogentili', 'message' => $e->getCode().': '.$e->getMessage().PHP_EOL.$e->getTraceAsString()]);
+        }
+
+        try {
+            if (isset($update['message']['media']) && ($update['message']['media']['_'] == 'messageMediaPhoto' || $update['message']['media']['_'] == 'messageMediaDocument')) {
+                $time = microtime(true);
+                $file = $this->download_to_dir($update, '/tmp');
+                $this->messages->sendMessage(['peer' => $update, 'message' => 'Downloaded to '.$file.' in '.(microtime(true) - $time).' seconds', 'reply_to_msg_id' => $update['message']['id'], 'entities' => [['_' => 'messageEntityPre', 'offset' => 0, 'length' => strlen($res), 'language' => 'json']]]);
+            }
+        } catch (\danog\MadelineProto\RPCErrorException $e) {
+            $this->messages->sendMessage(['peer' => '@danogentili', 'message' => $e->getCode().': '.$e->getMessage().PHP_EOL.$e->getTraceAsString()]);
+        }
+    }
+}
