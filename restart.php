3bb584a8ed9829a6c210ff8ff04108f9470e50b4
Ammar Faizi <ammarfaizi2@gmail.com>
2018-11-11 21:18:08 +0700

    Update restart.php

--- restart.php
+++ restart.php
@@ -7,4 +7,4 @@ if (file_exists(PID_FILE)) {
 	shell_exec("kill -9 {$pid}");
 }
 
-shell_exec("nohup /usr/bin/env php ".__DIR__."/bin/ibld >> ".__DIR__."/storage/logs/ibld.log 2>&1 &");
+// shell_exec("nohup /usr/bin/env php ".__DIR__."/bin/ibld >> ".__DIR__."/storage/logs/ibld.log 2>&1 &");
